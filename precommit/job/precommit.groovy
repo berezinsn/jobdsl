5b89498f7baa76be12f414bf9e25fe68eb76d7a1
Sergey Berezin <update.berezin@gmail.com>
2019-08-07 18:52:48 +0400

    Additional changes

--- precommit/job/precommit.groovy
+++ precommit/job/precommit.groovy
@@ -25,11 +25,8 @@ job('petclinic/PR-build') {
             permitAll()
             extensions {
                 commitStatus {
-                    context('deploy to staging site')
                     triggeredStatus('starting deployment to staging site...')
                     startedStatus('deploying to staging site...')
-                    addTestResults(true)
-                    statusUrl('http://mystatussite.com/prs')
                     completedStatus('SUCCESS', 'All is well')
                     completedStatus('FAILURE', 'Something went wrong. Investigate!')
                     completedStatus('PENDING', 'still in progress...')
