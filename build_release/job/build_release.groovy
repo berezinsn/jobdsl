208fdce084b494e8c6a123708bf882d994632ffb
Sergey Berezin <update.berezin@gmail.com>
2019-08-16 17:28:14 +0400

    Release job has been added

---
+++ build_release/job/build_release.groovy
@@ -0,0 +1,58 @@
+job('Build-Release') {
+    label('slave')
+    wrappers {
+        maskPasswords()
+        colorizeOutput()
+        timestamps()
+        preBuildCleanup()
+        credentialsBinding {
+            usernamePassword {
+                // Name of an environment variable to be set to the username during the build.
+                usernameVariable('LOGIN')
+                // Name of an environment variable to be set to the password during the build.
+                passwordVariable('PASS')
+                // Credentials of an appropriate type to be set to the variable.
+                credentialsId('REGISTRY')
+            }
+        }
+    }
+    properties {
+        githubProjectProperty {
+            // Mandatory part
+            projectUrlStr('https://github.com/berezinsn/spring-petclinic/')
+        }
+    }
+    scm {
+        git {
+            remote {
+                credentials('GIT')
+                url('https://github.com/berezinsn/spring-petclinic.git')
+                refspec('+refs/pull/*:refs/remotes/origin/release-*.*')
+            }
+            branch('${sha1}')
+            extensions {
+                wipeOutWorkspace()
+            }
+        }
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        // Secured docker registry authentication with bind credentials .
+        shell(readFileFromWorkspace('shell/docker_login.sh'))
+        // Generation of the file with the combined version.
+        shell(readFileFromWorkspace('shell/build_version.sh'))
+        envInjectBuilder {
+            propertiesFilePath('env.properties')
+            propertiesContent('')
+        }
+        maven {
+            goals('versions:set -B')
+            property('newVersion', '${VERSION}')
+        }
+        maven {
+            goals('clean deploy -Pdocker -B')
+        }
+    }
+}
