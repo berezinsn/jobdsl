b8ff1c288104d9949803fc2e729f8dcd146381e7
Sergey Berezin <update.berezin@gmail.com>
2019-09-11 17:43:09 +0400

    Added the Git commit variable

--- shell/build_version.sh
+++ shell/build_version.sh
@@ -3,3 +3,4 @@ TIMESTAMP=$(date "+%Y%m%d.%H%M%S")
 GIT_HASH=$(git log -1 --pretty=%h)
 MAVEN_UPDATED_PROJECT_VERSION="${MAVEN_PROJECT_VERSION}-${TIMESTAMP}-${GIT_HASH}"
 echo "VERSION=${MAVEN_UPDATED_PROJECT_VERSION}" >> env.properties
+echo "GIT_COMMIT=${GIT_COMMIT}" >> env.properties
