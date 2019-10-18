8ff280cf98ee38238e301a03ff56faf094aaf4f2
Sergey Berezin <update.berezin@gmail.com>
2019-10-18 14:09:13 +0400

    Link

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -11,4 +11,4 @@ kubectl get pods
 curl -X GET -u ${LOGIN}:${PASS} https://nexus.ci-cd.ru/repository/tgz/${VERSION} -O
 helm init --client-only
 helm install ${VERSION}
-sleep 60 && echo Public IP $(kubectl get svc | grep haproxy | awk '{print $4}'):443
+sleep 60 && echo Endpoint link: https://$(kubectl get svc | grep haproxy | awk '{print $4}'):443
